e288fcbbaceb3f288669a7df78385d79af7ae728
Mario Cortes <mcortes.1113@gmail.com>
2020-01-10 09:58:12 -0600

    code cleanup

--- server.js
+++ server.js
@@ -17,31 +17,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-app.get("/api/workouts", (req, res) => {
-  db.Workout.find({}).then(function(workouts) {
-    // console.log(req);
-    // console.log(res);
-    res.json(workouts);
-  }).catch(err => {
-    console.log(err);
-  });
-});
-
-app.put("api/workouts", (req, res) => {
-
-})
-
-app.post("api/workouts", (req, res) => {
-
-})
-
-app.get("api/workouts/range", (req, res) => {
-  
-})
-
-app.get("/", (req, res) => {
-  
-})
+app.use(require("./routes/apiroutes.js"));
+app.use(require("./routes/htmlroutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
