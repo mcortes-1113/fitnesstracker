8269a0c14b8c25e85a28385af5704657897ed3e9
Mario Cortes <mcortes.1113@gmail.com>
2020-01-10 09:57:22 -0600

    code cleanup

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,10 +2,6 @@ var path = require("path");
 
 module.exports = function (app) {
 
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, '..','/public/index.html'));
-    });
-
     app.get("/stats", function (req, res) {
         res.sendFile(path.join(__dirname, '..','/public/stats.html'));
     });
@@ -13,4 +9,9 @@ module.exports = function (app) {
     app.get("/exercise", function (req, res) {
         res.sendFile(path.join(__dirname, '..','/public/exercise.html'));
     });
+
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, '..','/public/index.html'));
+    });
+    
 }
