386e53cb864bbec56062a131ec0ca9ad8725def6
Mario Cortes <mcortes.1113@gmail.com>
2020-01-12 14:48:46 -0600

    modified mongoose connection

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
 const PORT = process.env.PORT || 4000;
-
 const db = require("./models/");
-
 const app = express();
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
 
 app.use(logger("dev"));
 
@@ -15,7 +14,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.use(require("./routes/apiroutes.js"));
 app.use(require("./routes/htmlroutes.js"));
