80a6f1e68e67e3678211aa2e7e2f748fef1da9c7
Mario Cortes <mcortes.1113@gmail.com>
2020-01-11 23:24:40 -0600

    Updated html and api routes

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,17 +1,19 @@
+var router = require("express").Router();
 var path = require("path");
 
-module.exports = function (app) {
+router.get("/stats", function (req, res) {
+    console.log("stats route");
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
 
-    app.get("/stats", function (req, res) {
-        res.sendFile(path.join(__dirname, '..','/public/stats.html'));
-    });
+router.get("/exercise", function (req, res) {
+    console.log("made it");
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
 
-    app.get("/exercise", function (req, res) {
-        res.sendFile(path.join(__dirname, '..','/public/exercise.html'));
-    });
+router.get("/", function (req, res) {
+    console.log("home");
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, '..','/public/index.html'));
-    });
-    
-}
+module.exports = router;
